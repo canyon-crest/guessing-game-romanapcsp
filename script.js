09af45460b3a183bb3e22b4f3f94b01c6ec63a06
romanapcsp <soobbenr6929@my.sduhsd.net>
2026-04-08 22:23:09 +0000

    push i guess

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 // add javascript here
-let guess = 0;
 let answer = 0;
 let guessCount = 0;
-let totalWins = 0;
 const scores = [];
 
 document.getElementById("playBtn").addEventListener("click",play);
+document.getElementById("guessBtn").addEventListener("click", makeGuess);
 
 function play(){
     let range = 0;
@@ -23,4 +22,51 @@ function play(){
     guessBtn.disabled = false;
     giveUpBtn.disabled = false;
     playBtn.disabled = true;
+}
+
+function makeGuess(){
+    let guess = parseInt(document.getElementById("guess").value);
+    if(isNaN(guess)){
+        msg.textContent = "Please enter a valid number";
+        return;
+    }
+    guessCount++;
+    if(guess == answer){
+        msg.textContent = "Correct! It took " + guessCount + " tries.";
+        updateScore(guessCount);
+        reset();
+    }
+    else if(guess < answer){
+        msg.textContent = "Too low, try again.";
+    }
+    else{
+        msg.textContent = "Too high, try again."
+    }
+}
+
+function updateScore(score){
+    scores.push(score);
+    wins.textContent = "Total wins: "+ score.length;
+    let sum =0;
+    for(let i = 0; i < scores.length; i++){
+        sum += scores[i];
+    }
+    avgScore.textContent = "Average Score: " + (sum/scores.length).toFixed(1);
+
+    scores.sort(function(a,b){return a-b;});
+    let lb = document.getElementsByName("leaderboard");
+    for(let i = 0; i < lb.length; i++){
+        if(i < scores.length){
+            lb[i].textContent = scores[i];
+        }
+    }
+}
+function resetGame(){
+    guess.value = "";
+    guessBtn.disabled = true;
+    giveUpBtn.disabled = true;
+    playBtn.disabled = false;
+    e.disabled = false;
+    m.disabled = false;
+    h.disabled = false;
 }
